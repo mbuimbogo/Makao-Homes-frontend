a874cf2a0f18ecfc0f1bb44061133fa8144b8ec9
luyosamson <luyosamson@gmail.com>
2022-12-27 02:39:08 +0300

    Modify App.js for testing

--- makao-homes/src/App.js
+++ makao-homes/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import AddProperty from './components/AddProperty';
 
 function App() {
   return (
     <div className="App">
+      <h1>This is a test</h1>
+      <AddProperty />
         </div>
   );
 }
