abe459af4ddd8d9921223dae2fb21913476e4cf1
luyosamson <luyosamson@gmail.com>
2022-12-27 13:32:13 +0300

    Add events and states

--- makao-homes/src/components/AddProperty.js
+++ makao-homes/src/components/AddProperty.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function AddProperty() {
+  const [name,setName]=useState('')
+  const [location,setLocation]=useState('')
+  const[price,setPrice]=useState('')
+  const[ImageURL,setImageURL]=useState('')
+  const[description,setDescription]=useState('')
+  const[features,setFeature]=useState('')
 
 
   function handleFormSubmit(e){
     e.preventDefault()
+    const propertyData={
+      name:name,
+      location:location,
+      price:price,
+      ImageURL:ImageURL,
+      description:description,
+      features:features,
+    }
+    // console.log(propertyData)
+    fetch(' http://localhost:4000/newproperty',{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify(propertyData)
+
+    })
+    .then((r)=>r.json())
+    .then((newData)=>console.log(newData))
 
   }
   return (
@@ -12,12 +37,12 @@ function AddProperty() {
       <div className='addProperty'>
       <h3 id='sellTitle'>Sell With Us</h3>
     <form onSubmit={handleFormSubmit}>
-        <label>Property Name</label><input type="text" />
-        <label>Location</label><input type="text" />
-        <label>Price</label><input type="text" />
-        <label>Image URL</label><input type="text" />
-        <label>Description</label><textarea></textarea>
-        <label>Features</label><textarea></textarea>
+        <label>Property Name</label><input type="text" value={name} onChange={(e)=>setName(e.target.value)}/>
+        <label>Location</label><input type="text" value={location} onChange={(e)=>setLocation(e.target.value)}/>
+        <label>Price</label><input type="number" value={price} onChange={(e)=>setPrice(e.target.value)} />
+        <label>Image URL</label><input type="text" value={ImageURL} onChange={(e)=>setImageURL(e.target.value)} />
+        <label>Description</label><textarea input="true" value={description} onChange={(e)=>setDescription(e.target.value)}></textarea>
+        <label>Features</label><textarea input="true" value={features} onChange={(e)=>setFeature(e.target.value)}></textarea>
         <button type='submit'>Submit</button>
 
 
