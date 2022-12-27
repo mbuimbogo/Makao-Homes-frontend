b03e27a0f8672a6228470b795a68df91f5ed6980
luyosamson <luyosamson@gmail.com>
2022-12-27 05:33:07 +0300

    Add submit button

--- makao-homes/src/components/AddProperty.js
+++ makao-homes/src/components/AddProperty.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
 function AddProperty() {
+
+
+  function handleFormSubmit(e){
+    e.preventDefault()
+
+  }
   return (
     <div>
       <div className='addProperty'>
       <h3 id='sellTitle'>Sell With Us</h3>
-      <form>
+    <form onSubmit={handleFormSubmit}>
         <label>Property Name</label><input type="text" />
         <label>Location</label><input type="text" />
         <label>Price</label><input type="text" />
