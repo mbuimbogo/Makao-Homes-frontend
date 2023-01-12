38ed26bc3c7d5a095a41745dd24e16fa6947ce7e
ferdinandmukodo <ferdinant.mukodo@student.moringaschool.com>
2023-01-12 09:05:21 +0300

    install bootstrap

--- makao-homes/src/App.js
+++ makao-homes/src/App.js
@@ -3,13 +3,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
+import Login from "./components/Login"
+import "./App.css"
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        {/* <Route path="/login" element={<Login />} /> */}
+        <Route path="/login" element={<Login />} /> 
         {/* <Route path="/" element={<Home />} /> */}
         {/* <Route path="/signup" element={<Signup />} /> */}
         {/* <Route path="/resetpassword" element={<Forgot />} /> */}
