6b9e854e579744d68543d6bf09b6eaa1b03413e8
ferdinandmukodo <ferdinant.mukodo@student.moringaschool.com>
2023-01-12 10:29:05 +0300

    password reset

--- makao-homes/src/App.js
+++ makao-homes/src/App.js
@@ -1,10 +1,11 @@
-
-
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Login from "./components/Login"
+import Signup from "./components/Signup";
+import Forgot from "./components/Forgot"
 import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
@@ -13,8 +14,8 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} /> 
         {/* <Route path="/" element={<Home />} /> */}
-        {/* <Route path="/signup" element={<Signup />} /> */}
-        {/* <Route path="/resetpassword" element={<Forgot />} /> */}
+         <Route path="/signup" element={<Signup />} /> 
+        <Route path="/resetpassword" element={<Forgot />} /> 
       
       </Routes>
     </BrowserRouter>
