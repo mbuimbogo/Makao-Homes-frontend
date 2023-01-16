db2ba3db6c8a0bee1024516815ffe3267d9840e2
luyosamson <luyosamson@gmail.com>
2023-01-16 08:39:50 +0300

    Modify

---
+++ makao-homes/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+// import Navbar from "./components/navbar/Navbar";
+// import Hero from "./components/landing/Hero";
+// import Good from "./components/hands/Good";
+// import Featured from "./components/featured/Featured";
+// import You from "./components/you/You";
+// import Testimonial from "./components/testimony/Testimonial";
+// import AddProperty from "./components/AddProperty";
+import Checkout from "./components/Checkout";
+
+
+
+function App() {
+  return (
+    <>
+      {/* <Navbar />
+      <Hero />
+      <Good />
+      <Featured />
+      <You />
+      <Testimonial /> */}
+      {/* <AddProperty/> */}
+      <Checkout/>
+
+
+    </>
+  );
+}
+
+export default App;
