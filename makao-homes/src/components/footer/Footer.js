3b24b139f26a0eb97e8f71816301681d93770a10
Leo <leomutungi19@gmail.com>
2023-01-18 15:45:20 +0300

    create and style footer

--- makao-homes/src/components/footer/Footer.js
+++ makao-homes/src/components/footer/Footer.js
@@ -1,19 +1,52 @@
 import React from "react"
+// import { useNavigate } from "react-router-dom"
 import { footer } from "../../data"
 import "./Footer.css"
 import {BsFillHouseFill} from 'react-icons/bs'
+import { Container } from "react-bootstrap"
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook,faInstagram, faTwitter, faTiktok, faYoutube } from '@fortawesome/free-brands-svg-icons'
+
 
 
 const Footer = () => {
+
+
   return (
     <>
-      <section className='footerContact'>
+    <section className='footerContact'>
         <div className='container'>
           <div className='send flex'>
           
             <div className='text'>
               <h1>have any questions ?</h1>
+
+              <div className="singleCol social-media-icons-white d-flex justify-content-evenly">
+                <a href="https://facebook.com/home.php">
+                  <FontAwesomeIcon icon={faFacebook} />
+                </a>
+
+                <a href="https://instagram.com">
+                  <FontAwesomeIcon icon={faInstagram} />
+                </a>
+
+                <a href="https://twitter.com">
+                  <FontAwesomeIcon icon={faTwitter} />
+                </a>
+
+                <a href="https://tiktok.com">
+                  <FontAwesomeIcon icon={faTiktok} />
+                </a>
+
+                <a href="https://youtube.com">
+                  <FontAwesomeIcon icon={faYoutube} />
+                </a>
+
+              </div>
+
               <p>Your Gateway to a richer life where your dreams come home.</p>
+              <a href="/sellwithus"><button className="btn">Sell With Us</button></a>
             </div>
           </div>
           
@@ -28,7 +61,7 @@ const Footer = () => {
               <h2>Make Your Dreams a Reality</h2>
               
 
-              <button className="btn">Sell With Us</button>
+              
             </div>
           </div>
 
