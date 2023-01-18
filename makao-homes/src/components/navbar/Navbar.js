5eadc33430cac4afd9590acda97937cef0df7189
Leo <leomutungi19@gmail.com>
2023-01-18 15:45:51 +0300

    style nav bar position

--- makao-homes/src/components/navbar/Navbar.js
+++ makao-homes/src/components/navbar/Navbar.js
@@ -1,4 +1,6 @@
 import React, {useState} from "react";
+// import { useNavigate } from "react-router-dom";
+
 import {HiOutlineMenuAlt4} from 'react-icons/hi';
 import {FaRegTimesCircle} from 'react-icons/fa' ;
 import {BsFillHouseFill} from 'react-icons/bs'
@@ -6,31 +8,46 @@ import './Navbar.css'
 
 const Navbar = () => {
 
+
+    // const navigate = useNavigate ()
+
+    // const navigateToSignup = () => {
+    //   navigate("/signup")
+    // }
+
     const handleClick = () => setClick(!click)
     const [click, setClick] = useState(false)
 
+    
+
     return (
-        <div className="navbar">
-            <div className="container">
-                <h1><span><BsFillHouseFill/>Makao</span>Homes</h1>
+        <>
+            <div className="navbar">
+                <div className="container">
+                    <h1><span><BsFillHouseFill/>Makao</span>Homes</h1>
 
-                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">About</a></li>
-                    <li><a href="">Contact</a></li>
-                    <li><a href="">Sell With Us</a></li>
-                </ul>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/properties">Properties</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/contact">Contact</a></li>
+                        <li><a href="/sellwithus">Sell With Us</a></li>
+                        <li><a href="/signup" ><button className="btn" >Sign Up</button></a></li>
+                        
+                    </ul>
 
-                <button className="btn">Sign Up</button>
-
-                <div className="hamburger" onClick={handleClick}>
-                    {click ? (<FaRegTimesCircle className="icon" />) : (<HiOutlineMenuAlt4 className="icon" />)}
                     
+
+                    <div className="hamburger" onClick={handleClick}>
+                        {click ? (<FaRegTimesCircle className="icon" />) : (<HiOutlineMenuAlt4 className="icon" />)}
+                        
+                    </div>
+
                 </div>
 
             </div>
-
-        </div>
+            
+        </>
     )
 }
 
